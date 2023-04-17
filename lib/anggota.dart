f236c4541d6cacc6a3413ba521d16421361479f1
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-04-18 00:56:29 +0700

    Update UI daftar anggota

--- lib/anggota.dart
+++ lib/anggota.dart
@@ -1,56 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:tugas4/azka.dart';
-import 'azka.dart';
-import 'raja.dart';
-import 'ryzal.dart';
 
-class Anggota extends StatelessWidget {
+class AnggotaPage extends StatelessWidget {
+  const AnggotaPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Daftar Anggota'),
+        title: Text('Anggota'),
+        centerTitle: true,
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        children: [
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
+      backgroundColor: Colors.grey.shade200,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildAnggotaCard(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => Azka(),
-                ),
-              );
-            },
-            child: Text('Azka'),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
+                'Muhammad Azka Hartami',
+                '1232000051',
+                'assets/azka.jpg',
+              ),
+              _buildAnggotaCard(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => Raja(),
-                ),
-              );
-            },
-            child: Text('Raja'),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
+                'Ryzal Fadhillah',
+                '123200053',
+                'assets/ryzal.jpg',
+              ),
+              _buildAnggotaCard(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => Ryzal(),
-                ),
-              );
-            },
-            child: Text('Ryzal'),
+                'Raja Pranata Panjaitan',
+                '123200060',
+                'assets/raja.jpg',
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnggotaCard(
+    BuildContext context,
+    String name,
+    String nim,
+    String imagePath,
+  ) {
+    return Container(
+      width: double.infinity,
+      child: Card(
+        elevation: 4.0,
+        margin: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(16.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage(imagePath),
+                radius: 60.0,
+              ),
+            ),
+            Text(
+              name,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'NIM: $nim',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -36,7 +36,7 @@ class HomeScreen extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => Anggota(),
+                    builder: (context) => AnggotaPage(),
                   ),
                 );
               },
